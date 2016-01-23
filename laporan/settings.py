539e8eb0c0fe31d1847864322ce0e9e96fc409c9
Irfan Fadhila <irfanfadhila@Irfans-MacBook-Pro.local>
2016-01-23 08:10:23 +0700

    deleted some

--- laporan/settings.py
+++
@@ -1,6 +0,0 @@
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    'laporan/templates',
-)
